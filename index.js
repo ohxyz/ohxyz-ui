09f464df09b3007fd5d5373611cd5a2e71d19dbe
Oh Xyz <ohxyzcom@gmail.com>
2018-03-27 16:33:25 +1100

    Fixed path, src -> lib

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-import InsertBase from './src/controls/insert-base.js';
-import InsertText from './src/controls/insert-text.js';
-import InsertNumber from './src/controls/insert-number.js';
-import InsertTextArea from './src/controls/insert-textarea.js';
-import InsertDateBase from './src/controls/insert-date-base.js';
-import InsertDateByText from './src/controls/insert-date-by-text.js';
-import InsertDateByNumber from './src/controls/insert-date-by-number.js';
+import InsertBase from './lib/controls/insert-base.js';
+import InsertText from './lib/controls/insert-text.js';
+import InsertNumber from './lib/controls/insert-number.js';
+import InsertTextArea from './lib/controls/insert-textarea.js';
+import InsertDateBase from './lib/controls/insert-date-base.js';
+import InsertDateByText from './lib/controls/insert-date-by-text.js';
+import InsertDateByNumber from './lib/controls/insert-date-by-number.js';
 
-import OptionsBase from './src/controls/options-base.js';
-import OptionsList from './src/controls/options-list.js';
-import OptionsRadioList from './src/controls/options-radio-list.js';
-import OptionsCheckboxList from './src/controls/options-checkbox-list.js';
+import OptionsBase from './lib/controls/options-base.js';
+import OptionsList from './lib/controls/options-list.js';
+import OptionsRadioList from './lib/controls/options-radio-list.js';
+import OptionsCheckboxList from './lib/controls/options-checkbox-list.js';
 
-import Dropdown from './src/controls/dropdown.js';
-import DropdownList from './src/controls/dropdown-list.js';
+import Dropdown from './lib/controls/dropdown.js';
+import DropdownList from './lib/controls/dropdown-list.js';
 
-import Submit from './src/controls/submit.js';
+import Submit from './lib/controls/submit.js';
 
 /* A wrapper contains a title, a main form element and error messages */
 
-import FormControl from './src/form-controls/form-control.js';
+import FormControl from './lib/form-controls/form-control.js';
 
-import InsertTextControl from './src/form-controls/insert-text-control.js';
-import InsertNumberControl from './src/form-controls/insert-number-control.js';
-import InsertTextAreaControl from './src/form-controls/insert-textarea-control.js';
-import InsertDateByTextControl from './src/form-controls/insert-date-by-text-control.js';
-import InsertDateByNumberControl from './src/form-controls/insert-date-by-number-control.js';
-import OptionsListControl from './src/form-controls/options-list-control.js';
-import OptionsRadioListControl from './src/form-controls/options-radio-list-control.js';
-import OptionsCheckboxListControl from './src/form-controls/options-checkbox-list-control.js';
-import SubmitControl from './src/form-controls/submit-control.js';
+import InsertTextControl from './lib/form-controls/insert-text-control.js';
+import InsertNumberControl from './lib/form-controls/insert-number-control.js';
+import InsertTextAreaControl from './lib/form-controls/insert-textarea-control.js';
+import InsertDateByTextControl from './lib/form-controls/insert-date-by-text-control.js';
+import InsertDateByNumberControl from './lib/form-controls/insert-date-by-number-control.js';
+import OptionsListControl from './lib/form-controls/options-list-control.js';
+import OptionsRadioListControl from './lib/form-controls/options-radio-list-control.js';
+import OptionsCheckboxListControl from './lib/form-controls/options-checkbox-list-control.js';
+import SubmitControl from './lib/form-controls/submit-control.js';
 
 /* Others */
-import dataType from './src/datatype.js';
+import dataType from './lib/datatype.js';
 
 export {
 
